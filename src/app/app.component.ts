cfb3c7e65568d61eb060596f7e877315290cab0d
Jesus Hinojosa <gnu.linux.jesus@gmail.com>
2018-05-12 16:29:04 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3{
+    color: dodgerBlue;
+  }`],
 })
 export class AppComponent {
-  title = 'app';
+  public x = 'hello ';
+
+  public hello = (name: string ) => {
+    return this.x + name;
+  }
 }
